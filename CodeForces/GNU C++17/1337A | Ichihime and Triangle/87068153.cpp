59b5c001e503c4b863266cc891a39d072bf08d98
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:18:13 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1337A | Ichihime and Triangle/87068153.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(0);
+ cout.tie(0);
+ 
+ int tc;
+ cin >> tc;
+ while(tc--)
+ {
+  int a, b, c,d ;
+  cin >> a >> b >> c >> d;
+  cout << b << " " << c << " " << c << '\n';
+ 
+ }
+}
