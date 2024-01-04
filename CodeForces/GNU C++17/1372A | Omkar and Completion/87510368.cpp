0368d465c9cc1ee820d9a95fad7d974b578658ce
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:15:20 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1372A | Omkar and Completion/87510368.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(0);
+ cout.tie(0);
+ 
+ int tc;
+ cin >> tc;
+ while(tc--)
+ {
+  int n;
+  cin >> n;
+  for(int i = 0; i < n; i++)
+  {
+   if(i != 0)
+   {
+    cout << " ";
+   }
+   cout << 1;
+ 
+  }
+  cout << '\n';
+ }
+ 
+}
