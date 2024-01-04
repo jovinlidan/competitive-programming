d336e5ca6d35d5ce28751adc32b4dafd6fd08645
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:45:51 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1096A | Find Divisible/68905424.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ long long int t;
+ cin >> t;
+ while(t--)
+ {
+  long long int a, b;
+  cin >> a >> b;
+  cout << a << " " << 2 * a << endl;
+ 
+ 
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
