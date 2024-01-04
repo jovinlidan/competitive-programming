8bd945bc0aab483fb9fc4e16eb5caf053b1f9e7d
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:38:30 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1093A | Dice Rolling/69402215.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+ int tc;
+ cin >> tc;
+ while(tc--)
+ {
+  int n;
+  cin >> n;
+  cout <<ceil(n/7.0) << endl;
+ }
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
