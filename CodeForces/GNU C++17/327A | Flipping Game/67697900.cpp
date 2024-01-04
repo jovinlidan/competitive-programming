450fd347259dda8e893704452e89e6db67ee9add
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:48:38 +0700

    Committing files

---
+++ CodeForces/GNU C++17/327A | Flipping Game/67697900.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int DP[105][105] = {}, arr[105] = {};
+ 
+int hasil = -1e9;
+void solve(int i, int j, int N)
+{
+    //if(DP[i][j] != 0) return DP[i][j];
+    if(j > N){
+        i++;
+        j = i;
+    }
+    if(i > N){
+        return ;
+    }
+    int ans = 0;
+    for(int a = i; a <= j; a++){
+        ans += (1 - arr[a]);
+    }
+    for(int a = 1; a < i; a++){
+        //cout<< i << " " << j << endl;
+        ans += arr[a];
+    }
+    for(int a = j+1; a <= N; a++){
+        ans+= arr[a];
+    }
+    //cout << ans << endl;
+    //cout << i << " " << j<< " HASIL : " << ans <<  endl;
+    if(ans <= N) hasil = max(ans, hasil);
+ 
+ 
+    solve(i, j+=1, N);
+ 
+}
+int main()
+{
+    int N;
+    cin >> N;
+    for(int i =1 ;i <= N; i++){
+        cin >> arr[i];
+    }
+ 
+    solve(1, 1, N);
+    cout << hasil << endl;
+ 
+ 
+ 
+ 
+    return 0;
+}
