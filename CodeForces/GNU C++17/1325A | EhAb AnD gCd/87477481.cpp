93302facf37af8b3bbfbc02ad21c27b29c669321
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:15:44 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1325A | EhAb AnD gCd/87477481.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int lcm(int a, int b)
+ {
+    return (a*b)/__gcd(a, b);
+ }
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(0);
+ cout.tie(0);
+ 
+ int tc;
+ cin >> tc;
+ while(tc--)
+ {
+  int a, b;
+  cin >> a;
+  cout << a-1 << " " << 1 << '\n';
+ 
+ 
+ }
+ 
+}
