c15d4ce4be59787638873bc1e69399a3b81e90f7
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:36:34 +0700

    Committing files

---
+++ CodeForces/GNU C++17/805A | Fake NP/70120092.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+int main()
+{
+ int x, y;
+ cin >> x >> y;
+ if(x-y  == 0 || x-y == 1) cout << x << endl;
+ else cout << 2 << endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
