72817051d8393088f86ddbf80e7b94a455c3fbf3
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:12:19 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1345A | Puzzle Pieces/87689644.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(0);
+ cout.tie(0);
+ 
+ int tc;
+ cin >> tc;
+ while(tc--)
+ {
+  int n, m;
+  cin>> n >> m;
+  if(n == 1 || m == 1 ||(n==2 && m == 2))cout << "YES\n";
+  else cout << "NO\n";
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+}
