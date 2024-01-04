f2d053810acd4c8c326df20c40d36909cdc1ff62
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 17:04:19 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1095A | Repeating Cipher/65265275.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+ int i, j, n, counts =0, patokan=1;
+ cin >> n;
+ string kata;
+ cin >> kata;
+ for (i = 0; i < n; i+=patokan)
+ {
+  cout << kata[i];
+  patokan++;
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
