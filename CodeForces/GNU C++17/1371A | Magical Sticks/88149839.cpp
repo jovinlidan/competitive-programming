d55ee266ba2209340b92eb54a5dcf8022534f1ef
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:07:29 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1371A | Magical Sticks/88149839.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+ 
+ 
+int main()
+{
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int n;
+        cin >> n;
+        cout << int(ceil(n/2.0))<< '\n';
+    }
+    return 0;
+}
