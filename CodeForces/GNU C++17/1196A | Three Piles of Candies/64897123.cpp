1f4d56f2e161113fa112832e42c4a0d83b47c960
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 17:05:00 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1196A | Three Piles of Candies/64897123.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+ unsigned long long int n, temp1, temp2, temp3;
+ cin >> n;
+ for ( int i = 0; i < n; i++)
+ {
+  cin >> temp1 >> temp2 >> temp3;
+  cout << (temp1 + temp2 + temp3) / 2 << endl;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
