1787c6c769ef58d583ac4190c8ef5c78643ce0d5
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 15:44:23 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1433C | Dominant Piranha/97988775.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define mxN 10000005
+#define INF -1e9
+typedef pair<int, int> pii;
+typedef long long int ll;
+const int mod = 1e9+7;
+ 
+ll modpow(ll x, ll y, ll m)
+{
+    ll res = 1;
+    while(y > 0)
+    {
+        if(y & 1)
+            res = (res * x) % m;
+        x = (x * x) % m;
+        y >>= 1;
+    }
+ 
+    return res;
+}
+ 
+bool cmp(pii l, pii r)
+{
+    if(l.first == r.first)
+    {
+        return l.second > r.second;
+    }
+    return l.first < r.first;
+}
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+ 
+    int tc, n;
+    cin >> tc;
+    while(tc--)
+    {
+        int n, mx = -1e9;
+        cin >> n;
+        int arr[n+5] = {};
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            mx = max(mx, arr[i]);
+        }
+        int ans = -1;
+        for(int i = 1; i < n; i++)
+        {
+            if(arr[i] == mx && arr[i-1] < mx)ans = i;
+        }
+        for(int i = 0; i < n-1; i++)
+        {
+            if(arr[i] == mx && arr[i+1] < mx)ans = i;
+        }
+        cout << ( (ans == -1)? -1 : (ans+1) ) << '\n';
+ 
+ 
+    }
+ 
+ 
+    return 0;
+}
