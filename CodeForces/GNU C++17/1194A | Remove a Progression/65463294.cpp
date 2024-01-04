8e4d2757c17f03ecae27d4f28afdf01b22565b82
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 17:00:01 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1194A | Remove a Progression/65463294.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ int q, x, n, i;
+ cin >> q;
+ 
+ for (i = 0; i < q; i++)
+ {
+  cin >> n >> x;
+  cout << x * 2 << endl;
+ }
+ 
+ 
+ return 0;
+}
