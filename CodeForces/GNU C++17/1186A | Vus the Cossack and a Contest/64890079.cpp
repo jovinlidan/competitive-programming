1f799206df4bbe77af3e05133bb45b524d75b2ba
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 17:05:12 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1186A | Vus the Cossack and a Contest/64890079.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+ int n, pen, books;
+ cin >> n >> pen >> books;
+ 
+ if ((pen >= n) &&( books >= n)) {
+  cout << "YES\n";
+ }
+ else
+  cout << "NO\n";
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
