eeab4388610a0323ba9290485b5d0d7cf4095ef8
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:33:52 +0700

    Committing files

---
+++ CodeForces/GNU C++17/1269A | Equation/70598891.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+int main()
+{
+ lli n;
+ cin >> n;
+ if(n == 1) cout << 9 << " " << 8 << endl;
+ else cout << n * 3 << " " << n * 2 << endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
