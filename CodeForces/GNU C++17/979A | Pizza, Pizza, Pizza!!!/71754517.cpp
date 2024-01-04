42b4e22abbf0e0f712bee6faad8fe0f18aef3e9d
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:25:59 +0700

    Committing files

---
+++ CodeForces/GNU C++17/979A | Pizza, Pizza, Pizza!!!/71754517.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    unsigned long long int n;
+    cin >> n;
+    n += 1;
+ 
+ if(n == 1) cout << 0 << endl;
+ else
+ {
+  if(n % 2 == 0) cout << n / 2;
+  else cout << n << endl;
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
