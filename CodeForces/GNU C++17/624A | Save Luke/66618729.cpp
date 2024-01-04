7ba4cd5dedc167b1d51252c19e4fb820555941ed
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:50:58 +0700

    Committing files

---
+++ CodeForces/GNU C++17/624A | Save Luke/66618729.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+ long double d, l, v1, v2;
+ cin >> d >> l >> v1 >> v2;
+ 
+ cout << fixed << setprecision(16) << abs(l - d)/(v1+v2);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
