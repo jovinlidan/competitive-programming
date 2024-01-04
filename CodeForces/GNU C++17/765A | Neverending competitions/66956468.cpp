a6641d023aaf210c1e43fa17a57df427430f98f0
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 16:50:25 +0700

    Committing files

---
+++ CodeForces/GNU C++17/765A | Neverending competitions/66956468.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n, ans=0;
+ string flight, home;
+ cin >> n;
+ cin >> home;
+ for(int i =0; i < n; i++){
+  cin >>  flight;
+ }
+ if(n % 2 ==0) cout << "home\n";
+ else cout << "contest\n";
+ 
+ 
+ 
+ 
+    return 0;
+}
