b60f0d00153c093766d2c84373a041d6d8242308
Jovin Lidan <50620119+jovinlidan@users.noreply.github.com>
2024-01-04 17:08:55 +0700

    Committing files

---
+++ CodeForces/GNU C++14/281A | Word Capitalization/64418623.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+ 
+using namespace std;
+ 
+int main() {
+ 
+ string word;
+ cin >> word;
+ 
+ if (int(word[0]) >= 97) {
+  word[0] = char(int(word[0]) - 32);
+ }
+ 
+ cout << word << endl;
+ 
+ 
+ return 0;
+}
